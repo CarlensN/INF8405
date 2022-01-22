e56c7aad4e0f094719042e3b7a418f377ed16150
Carlens <carlens.nerese@polymtl.ca>
2022-01-22 16:34:52 -0500

    create game level fragment

--- app/src/main/java/com/example/myapplication/GameActivity.java
+++ app/src/main/java/com/example/myapplication/GameActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -9,6 +12,17 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        //setContentView(R.layout.activity_game);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentByTag("fragment_level");
+
+        if (fragment == null){
+            FragmentTransaction ft = fm.beginTransaction();
+            fragment = new LevelFragment();
+            ft.add(android.R.id.content, fragment, "fragment_level");
+            ft.commit();
+        }
+
     }
 }
