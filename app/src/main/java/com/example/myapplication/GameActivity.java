80842439b4886121ecfa12e22a4ae7eb7770e51f
Carlens <carlens.nerese@polymtl.ca>
2022-01-22 22:04:54 -0500

    modify game activity and level fragment

--- app/src/main/java/com/example/myapplication/GameActivity.java
+++ app/src/main/java/com/example/myapplication/GameActivity.java
@@ -1,28 +1,69 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.File;
 
 public class GameActivity extends AppCompatActivity {
 
+    private int _currentLevel;
+    private int _minMoves;
+    private int _record;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_game);
+        setContentView(R.layout.activity_game);
+        setPreviousNextVisibility(_currentLevel);
+    }
 
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentByTag("fragment_level");
+    public void setFirstLevel(){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        Fragment fragment = new LevelFragment();
+        ft.replace(android.R.id.content, fragment, "fragment_level");
+        ft.addToBackStack(null);
+        ft.commit();
+        _currentLevel = 1;
+    }
 
-        if (fragment == null){
-            FragmentTransaction ft = fm.beginTransaction();
-            fragment = new LevelFragment();
-            ft.add(android.R.id.content, fragment, "fragment_level");
-            ft.commit();
+    public void createLevel(File grid){
+
+    }
+
+    public void previous(View view) {
+    }
+
+    public void next(View view) {
+    }
+
+    public void pause(View view) {
+    }
+
+    public void setPreviousNextVisibility(int level){
+        switch (level){
+            case 1 :
+                findViewById(R.id.previous_button).setVisibility(View.GONE);
+                findViewById(R.id.next_button).setVisibility(View.VISIBLE);
+                break;
+            case 2:
+                findViewById(R.id.previous_button).setVisibility(View.VISIBLE);
+                findViewById(R.id.next_button).setVisibility(View.VISIBLE);
+                break;
+
+            case 3:
+                findViewById(R.id.previous_button).setVisibility(View.VISIBLE);
+                findViewById(R.id.next_button).setVisibility(View.GONE);
+                break;
         }
-
     }
 }
--- app/src/main/java/com/example/myapplication/LevelFragment.java
+++ app/src/main/java/com/example/myapplication/LevelFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,20 +29,11 @@ public class LevelFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LevelFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
-    public static LevelFragment newInstance(String param1, String param2) {
+    public static LevelFragment newInstance() {
         LevelFragment fragment = new LevelFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,10 +41,6 @@ public class LevelFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
